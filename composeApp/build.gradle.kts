ee3bb8486038e5a24dfedf29d5007e4ed49f8cd1
changjianfei <changjianfei@bytedance.com>
2025-08-25 21:46:23 +0800

    build: 添加 androidx.activity.compose 依赖
    添加 androidx.activity.compose 依赖以支持 Compose 相关功能

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
         }
         iosMain.dependencies {
 
